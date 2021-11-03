dca11556be0f7b5c92522c3ac8152595fd253965
Tommi Niittymies <tommi.niittymies@gmail.com>
2021-11-03 18:50:53 +0200

    Fixing stuff, exercise 6

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true
+    "node": true,
+    "dist": true
   },
   "extends": [
     "eslint:recommended",
