2ad5c6fab6f016293413e5de59f06dbdb7fbac53
Tommi Niittymies <tommi.niittymies@gmail.com>
2021-11-06 20:24:58 +0200

    Added another e2e test

--- cypress/integration/ee_tests.js
+++ cypress/integration/ee_tests.js
@@ -4,4 +4,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('details page can be clicked open', function() {
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
