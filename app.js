06ba35e6a9f278985c877a73a60f06e014e97fa9
Tommi Niittymies <tommi.niittymies@gmail.com>
2021-11-07 16:07:46 +0200

    Cosmetic change for the sake of branching

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
+app.get('/health', (req, res) => {  
   res.send('ok')
 })
+
+// muutos branchia varten
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
